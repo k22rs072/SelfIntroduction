c0f021521a739edfd430458f68581327de0e17dd
Shoichi SUGA <145628125+k22rs072@users.noreply.github.com>
2024-04-25 22:52:02 +0900

    Update

--- SelfIntroduction/Voice.swift
+++ SelfIntroduction/Voice.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct Voice: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VoiceButton()
     }
+    
+    private func VoiceButton() -> some View{
+        Button(action: {
+            
+        }, label: {
+            Text("voice")
+                .padding()//背景の四角の範囲を決めている,順序が大事、テキストに余白を足した分に背景が付いている
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))//背景の形と色を決めている
+                .font(.largeTitle)//文字のサイズ
+                .foregroundStyle(Color.gray)//文字の色
+        })
+    }
+
 }
 
 #Preview {
