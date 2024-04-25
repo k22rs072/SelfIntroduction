7cb1a24312c9285c6c6bde07b4a530f9e196ccca
Shoichi SUGA <145628125+k22rs072@users.noreply.github.com>
2024-04-25 17:09:38 +0900

    Update

--- SelfIntroduction/CounterView.swift
+++ SelfIntroduction/CounterView.swift
@@ -13,7 +13,12 @@ struct CounterView: View {
     
     var body: some View {
         Text("\(count)")
-        CountPlusButton()
+            .font(.largeTitle)
+        HStack{
+            CountPlusButton()
+            CountMinusButton()
+        }
+        
     }
     
     private func CountPlusButton() -> some View{
@@ -21,6 +26,13 @@ struct CounterView: View {
             count += 1
         }, label: {
             Text("+")
+                .padding()//背景の四角の範囲を決めている
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))//背景の形と色を決めている
+                .font(.largeTitle)//文字のサイズ
+                .foregroundStyle(Color.gray)//文字の色
+//            Rectangle()//長方形を描画
+//                    .fill(Color.blue)
+//                    .frame(width: 100%, height: 200)
         })
     }
     
@@ -29,6 +41,10 @@ struct CounterView: View {
             count -= 1
         }, label: {
             Text("-")
+                .padding()//背景の四角の範囲を決めている
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))//背景の形と色を決めている
+                .font(.largeTitle)//文字のサイズ
+                .foregroundStyle(Color.gray)//文字の色
         })
     }
 }
