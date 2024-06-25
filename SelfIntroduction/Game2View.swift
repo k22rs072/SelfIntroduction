731aa8b65dd77e43a793fdb165e3ce4d670bb25b
Shoichi SUGA <145628125+k22rs072@users.noreply.github.com>
2024-06-26 01:05:29 +0900

    update

--- SelfIntroduction/Game2View.swift
+++ SelfIntroduction/Game2View.swift
@@ -23,6 +23,8 @@ struct Game2View: View {
   
     var body: some View {
         GeometryReader {geometry in
+            let faceImageViewWidth = getScreenWidth(geometry)
+            let faceImageViewHeight = getScreenHeight(geometry)
             ZStack{
                 
 
@@ -51,8 +53,13 @@ struct Game2View: View {
 //                                
 //                            }
 //                    }
-                faceImageView(x: $x, y: $y, score: $score, timerIsRunning: $timerIsRunning, size: size)
-                    .frame(height:geometry.size.height*0.7)
+                VStack{
+                    Spacer()
+                    faceImageView(x: $x, y: $y, score: $score, timerIsRunning: $timerIsRunning, size: size, player: $player)
+                        .frame(height:geometry.size.height*0.7)
+                    Spacer()
+                }
+               
                     
                     
                 
@@ -71,8 +78,7 @@ struct Game2View: View {
                         if remainingSeconds == 0   {
                             score = 0
                             remainingSeconds = 10
-//                            x = geometry2.frame(in: .local).maxX / 2
-//                            y = geometry2.frame(in: .local).maxY / 2
+                            _init(faceImageViewWidth, faceImageViewHeight)
                             
                         }else if remainingSeconds == 10{
                             
@@ -98,50 +104,62 @@ struct Game2View: View {
             
         }
     }
-    struct faceImageView: View {
-        @Binding var x: CGFloat
-        @Binding var y: CGFloat
-        @Binding var score: Int
-        @Binding var timerIsRunning: Bool
-        
-        var size: CGFloat
-        
-        var body: some View {
-            GeometryReader { geometry2 in
-                Image("face")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: size)
-                    .position(x: x, y: y)
-                    .onAppear() {
-                        x = geometry2.frame(in: .local).maxX / 2
-                        y = geometry2.frame(in: .local).maxY / 2
-                    }
-                    .onTapGesture {
-                        if timerIsRunning {
-                            playSound()
-                            x = CGFloat.random(in: geometry2.frame(in: .local).minX + 100...geometry2.frame(in: .local).maxX - 100)
-                            y = CGFloat.random(in: geometry2.frame(in: .local).minY + 100...geometry2.frame(in: .local).maxY - 100)
-                            score += 1
-                        } else {
-                            playSound2()
-                        }
-                    }
-            }
+
+    
+    private func getScreenWidth(_ geometry: GeometryProxy) -> CGFloat {
+            return geometry.size.width
         }
         
-        private func playSound() {
-            // サウンド再生のコード
-        }
-        
-        private func playSound2() {
-            // 別のサウンド再生のコード
+    private func getScreenHeight(_ geometry: GeometryProxy) -> CGFloat {
+        return geometry.size.height * 0.7
+    }
+    private func _init(_ faceImageViewWidth: CGFloat, _ faceImageViewHeight: CGFloat) {
+            x = faceImageViewWidth / 2
+            y = faceImageViewHeight / 2
+            remainingSeconds = 10
+            score = 0
+    }
+
+}
+struct faceImageView: View {
+    @Binding var x: CGFloat
+    @Binding var y: CGFloat
+    @Binding var score: Int
+    @Binding var timerIsRunning: Bool
+    var size: CGFloat
+    @Binding var player: AVAudioPlayer?
+    
+    var body: some View {
+        GeometryReader { geometry2 in
+            Image("face")
+                .resizable()
+                .scaledToFit()
+                .frame(width: size)
+                .position(x: x, y: y)
+                .onAppear() {
+                    x = geometry2.frame(in: .local).maxX / 2
+                    y = geometry2.frame(in: .local).maxY / 2
+                }
+                .onTapGesture {
+                    if timerIsRunning {
+                        playSound("voice2.mp3")
+                        x = CGFloat.random(in: geometry2.frame(in: .local).minX+size/2...geometry2.frame(in: .local).maxX - size/2)
+                        y = CGFloat.random(in: geometry2.frame(in: .local).minY + size/2...geometry2.frame(in: .local).maxY - size/2)
+                        score += 1
+                    } else {
+                        playSound("voice1.mp3")
+                    }
+                }
         }
     }
     
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "voice2", withExtension: "mp3") else { return }
-        
+    
+    func playSound(_ voice: String) {
+        let components = voice.split(separator: ".")//配列だよSubstring型だってさ初耳
+        guard components.count == 2,
+            let name = components.first,
+            let ext = components.last,
+            let url = Bundle.main.url(forResource: String(name), withExtension: String(ext)) else { return }
         do {
             player = try AVAudioPlayer(contentsOf: url)
             player?.play()
@@ -149,18 +167,10 @@ struct Game2View: View {
             print("Error playing sound: \(error.localizedDescription)")
         }
     }
-    func playSound２() {
-        guard let url = Bundle.main.url(forResource: "voice1", withExtension: "mp3") else { return }
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            player?.play()
-        } catch {
-            print("Error playing sound: \(error.localizedDescription)")
-        }
-    }
+    
+    
 }
-
+//separatorって面白い色々できそう
 #Preview {
     Game2View(size:256)
 }
